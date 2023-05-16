c0f6fc1452bc4305482b68f9459e0494ba8e867f
Evva28 <93273156+Evva28@users.noreply.github.com>
2023-05-16 19:39:15 +0200

    zarys logiki

---
+++ Assets/Logic/Board.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board
+{
+    public const int side = 9; // liczba pól na boku
+    //const int playerCount = 2; // liczba graczy
+    //const int checkerCount = 10; // liczba pionków gracza
+    public const int triangleSide = 4; // wielkość boku trójkąta gracza
+    public FieldInBoard[,] fields = new FieldInBoard[side, side];
+    //public Checker[,] checkers = new Checker[playerCount, checkerCount];
+
+    public Board ()
+    {
+        int checkerCounter = 0;
+        for (int i = 0; i < side; i++)
+        {
+            for (int j = 0; j < side; j++)
+            {
+                FieldInBoard newField = new FieldInBoard
+                {
+                    x = i,
+                    y = j
+                };
+                //Checker newChecker = null;
+
+                if (i < triangleSide && j < triangleSide)
+                {
+                    newField.fieldType = PlayerColor.Blue;
+                    newField.playerOnField = PlayerColor.Blue;
+                    //newChecker = new Checker (checkerCounter, PlayerColor.Blue);
+                    checkerCounter++;
+                }
+                else if (i >= side- triangleSide && j >= side- triangleSide)
+                {
+                    newField.fieldType = PlayerColor.Red;
+                    newField.playerOnField = PlayerColor.Red;
+                    //newChecker = new Checker (checkerCounter, PlayerColor.Red);
+                }
+                else
+                {
+                    newField.fieldType = PlayerColor.None;
+                    newField.playerOnField = PlayerColor.None;
+                }
+
+                fields[i, j] = newField;
+                //if (newChecker != null)
+                //{
+                //    newChecker.SetPosition (fields[i, j]);
+                //}
+            }
+        }
+    }
+}
+
+public class Checker
+{
+    public int ID { get; private set; }
+    public PlayerColor color { get; private set; }
+    public FieldInBoard placement { get; private set; }
+
+    public Checker (int id, PlayerColor color)
+    {
+        ID = id;
+        this.color = color;
+    }
+
+    public void SetPosition (FieldInBoard newPlace)
+    {
+        placement = newPlace;
+    }
+}
+
+public struct FieldInBoard
+{
+    public int x;
+    public int y;
+    public PlayerColor fieldType;
+    public PlayerColor playerOnField;
+}
+
+public enum PlayerColor
+{
+    None,
+    Red,
+    Blue
+}
---
+++ Assets/Logic/Game.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game //: MonoBehaviour
+{
+    public const int playerCount = 2;
+    public const int checkerCount = 10; // liczba pionków gracza
+
+    public Board board;
+    public Player[] players = new Player[playerCount];
+
+
+    public Game (PlayerType playerBlue, PlayerType playerRed)
+    {
+        // tworzymy graczy
+        switch (playerBlue)
+        {
+            case PlayerType.Human:
+                players[0] = new HumanPlayer (PlayerColor.Blue);
+                break;
+
+            case PlayerType.UCT:
+                players[0] = new UCTPlayer (PlayerColor.Blue);
+                break;
+
+            case PlayerType.RAVE:
+                players[0] = new RAVEPlayer (PlayerColor.Blue);
+                break;
+
+            case PlayerType.AUCT:
+                players[0] = new AUCTPlayer (PlayerColor.Blue);
+                break;
+
+            case PlayerType.Heuristic:
+                players[0] = new HeurPlayer (PlayerColor.Blue);
+                break;
+        }
+
+        switch (playerRed)
+        {
+            case PlayerType.Human:
+                players[1] = new HumanPlayer (PlayerColor.Red);
+                break;
+
+            case PlayerType.UCT:
+                players[1] = new UCTPlayer (PlayerColor.Red);
+                break;
+
+            case PlayerType.RAVE:
+                players[1] = new RAVEPlayer (PlayerColor.Red);
+                break;
+
+            case PlayerType.AUCT:
+                players[1] = new AUCTPlayer (PlayerColor.Red);
+                break;
+
+            case PlayerType.Heuristic:
+                players[1] = new HeurPlayer (PlayerColor.Red);
+                break;
+        }
+
+        // tworzymy planszę
+        board = new Board ();
+
+        // rozstawiamy pionki graczy
+        int checkerIndex = 0;
+        for (int i = 0; i < Board.triangleSide; i++)
+        {
+            for (int j = 0; j < Board.triangleSide; j++)
+            {
+                players[0].checkers[checkerIndex].SetPosition (board.fields[i, j]);
+                players[1].checkers[checkerIndex].SetPosition (board.fields[Board.side - i - 1, Board.side - j - 1]);
+                checkerIndex++;
+            }
+        }
+    }
+
+    public void Start ()
+    {
+
+    }
+}
