5c9231ceda5a45d40eb7b5a123626db53b7c9328
Evva28 <93273156+Evva28@users.noreply.github.com>
2023-05-21 13:16:32 +0200

    zarys mcts

--- Assets/Logic/Algorithms/UCTPlayer.cs
+++ Assets/Logic/Algorithms/UCTPlayer.cs
@@ -15,17 +15,40 @@ namespace Assets.Logic.Algorithms
 
         public override (FieldInBoard, int) MakeChoice (Board board, Player opponentStats)
         {
-            throw new System.NotImplementedException ();
-        }
-
-        public void GenerateChildren (Board board)
-        {
             //throw new System.NotImplementedException ();
 
             root = new Node (board, null);
             root.winningProbability = 0.5; // lub 0, jeszcze rozważyć
 
-            root.GenerateChildren ();
+            root.GenerateChildren (); // tworzymy startowe drzewo - póki co tylko dzieci root, każde z szansą 0.5 wygranej,
+                                      // chyba że akurat jest wygrana
+
+            while (true) // ileś loopów
+            {
+                Node leaf = Traverse (root); // wybieramy liść do rozbudowy
+                Node simulationResult = Rollout (leaf); // symulujemy rozgrywkę, zwracamy liść z wynikiem
+                Backpropagate (leaf, simulationResult); // aktualizujemy wartości w drzewie
+            }
+        }
+
+        private void Backpropagate (Node leaf, Node simulationResult)
+        {
+            throw new NotImplementedException ();
+        }
+
+        private Node Rollout (Node leaf)
+        {
+            throw new NotImplementedException ();
+        }
+
+        private Node Traverse (Node root)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public void GenerateChildren (Board board)
+        {
+            //throw new System.NotImplementedException ();
         }
     }
 }
