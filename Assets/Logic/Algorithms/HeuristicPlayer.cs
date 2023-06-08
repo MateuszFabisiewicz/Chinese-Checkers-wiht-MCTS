73b46166e7a3e1c4cca9bee8c4ea4447b33db2c2
Evva28 <93273156+Evva28@users.noreply.github.com>
2023-06-08 19:31:32 +0200

    komentarz z pomysłem

--- Assets/Logic/Algorithms/HeuristicPlayer.cs
+++ Assets/Logic/Algorithms/HeuristicPlayer.cs
@@ -18,6 +18,13 @@ namespace Assets.Logic.Algorithms
 
         public override (FieldInBoard, int, PlayerColor) MakeChoice (Board board, Player opponentStats)
         {
+            // pierwszy ruch: któryś z zewnętrznych kołków
+            // potem: chcemy ruszyć kołek który jeszcze jest w naszym trójkącie
+            // staramy się wybrać skok
+            // chcemy być blisko pozostałych kołków
+            // czyli każdy ruch będzie miał jakąś wartość h i będziemy wybierać ten z największą
+            // skok ma automatycznie 0.2, odległość od każdego pionka tuż obok to 0.1, a pole dalej to 0.05
+            // jeśli jeszcze nie było przesunięte z naszego trójkąta to 0.2
             throw new System.NotImplementedException ();
         }
 
