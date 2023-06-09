d88337b99b678f6d1a8ef8939ce8b9f072624c66
Evva28 <93273156+Evva28@users.noreply.github.com>
2023-06-09 12:49:18 +0200

    implementacja

--- Assets/Logic/Algorithms/HeuristicPlayer.cs
+++ Assets/Logic/Algorithms/HeuristicPlayer.cs
@@ -11,6 +11,14 @@ namespace Assets.Logic.Algorithms
 {
     public class HeuristicPlayer : Player
     {
+        private const int strongDist = 1;
+        private const int distance = 2;
+        private const double jumpH = 0.2;
+        private const double strongDistH = 0.1;
+        private const double distH = 0.05;
+        private const double triangleH = 0.2;
+        private const double opponentTriangleH = 0.15;
+
         public HeuristicPlayer (PlayerColor color) : base (color)
         {
             type = PlayerType.Heuristic;
@@ -25,8 +33,208 @@ namespace Assets.Logic.Algorithms
             // czyli każdy ruch będzie miał jakąś wartość h i będziemy wybierać ten z największą
             // skok ma automatycznie 0.2, odległość od każdego pionka tuż obok to 0.1, a pole dalej to 0.05
             // jeśli jeszcze nie było przesunięte z naszego trójkąta to 0.2
-            throw new System.NotImplementedException ();
+            // wejście w trójkąt przeciwnika to 0.15
+            // pójście "do przodu" jakoś uwzględnić?
+
+            bool isFirstMove = true;
+            List<(FieldInBoard newField, int checkerId, double heuristic)> possibleMoves = new List<(FieldInBoard newField, int checkerId, double heuristic)> ();
+
+
+            for (int i = 0; i < Board.side; i++)
+            {
+                for (int j = 0; j < Board.side; j++)
+                {
+                    if (board.fields[i, j].fieldType == color && board.fields[i, j].playerOnField != color)
+                    {
+                        isFirstMove = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isFirstMove)
+            {
+                // znajdujemy brzegowe checkery (indeksy [0,3], [3,0] lub [5,8][8,5])
+                // i ruszamy je na ukosy
+                if (board.fields[0, 3].fieldType == color)
+                {
+                    possibleMoves.Add ((board.fields[0, 4], board.fields[0, 3].checker.ID, 1));
+                    possibleMoves.Add ((board.fields[1, 3], board.fields[0, 3].checker.ID, 1));
+                    possibleMoves.Add ((board.fields[3, 1], board.fields[3, 0].checker.ID, 1));
+                    possibleMoves.Add ((board.fields[4, 0], board.fields[3, 0].checker.ID, 1));
+                }
+                else
+                {
+                    possibleMoves.Add ((board.fields[4, 8], board.fields[5, 8].checker.ID, 1));
+                    possibleMoves.Add ((board.fields[5, 7], board.fields[5, 8].checker.ID, 1));
+                    possibleMoves.Add ((board.fields[7, 5], board.fields[8, 5].checker.ID, 1));
+                    possibleMoves.Add ((board.fields[8, 4], board.fields[8, 5].checker.ID, 1));
+                }
+            }   
+            else
+            {
+                possibleMoves = GenerateMoves (board, opponentStats, color);
+            }
+
+            // zwracamy ruch z największą wartością heurystyki
+            possibleMoves.Sort ((x, y) => x.heuristic.CompareTo (y.heuristic));
+            return (possibleMoves[0].newField, possibleMoves[0].checkerId, color);
         }
 
+        private List<(FieldInBoard newField, int checkerId, double heuristic)> GenerateMoves (Board board, Player opponentStats, PlayerColor color)
+        {
+            List<(FieldInBoard, int, double)> moves = new List<(FieldInBoard, int, double)> ();
+
+            for (int i = 0; i < Game.checkerCount; i++)
+            {
+                // sprawdzamy czy możemy się ruszyć
+                List<FieldInBoard> possibleFieldsOneApart = GetPossibleFieldsOneApart (i, color, board);
+                foreach (FieldInBoard field in possibleFieldsOneApart)
+                {
+                    moves.Add ((field, i, CalculateHeuristic (field, i, board, false, color, board.FindCheckersPosition(i, color))));
+                }
+
+                List<FieldInBoard> possibleJumps = GetPossibleFieldsThroughJumps (board.FindCheckersPosition(i, color), board, board.FindCheckersPosition(i, color));
+                foreach (FieldInBoard field in possibleJumps)
+                {
+                    moves.Add ((field, i, CalculateHeuristic (field, i, board, true, color, board.FindCheckersPosition (i, color))));
+                }
+            }
+
+            return moves;
+        }
+
+        private List<FieldInBoard> GetPossibleFieldsThroughJumps (FieldInBoard startField, Board board, FieldInBoard previousPlace)
+        {
+            List<FieldInBoard> moves = new List<FieldInBoard> ();
+
+            moves.AddRange (OneJump (startField, board, (startField.x, startField.y + 2), (startField.x, startField.y + 1), previousPlace));
+            moves.AddRange (OneJump (startField, board, (startField.x, startField.y - 2), (startField.x, startField.y - 1), previousPlace));
+            moves.AddRange (OneJump (startField, board, (startField.x + 2, startField.y), (startField.x + 1, startField.y), previousPlace));
+            moves.AddRange (OneJump (startField, board, (startField.x - 2, startField.y), (startField.x - 1, startField.y), previousPlace));
+            moves.AddRange (OneJump (startField, board, (startField.x - 2, startField.y + 2), (startField.x - 1, startField.y + 1), previousPlace));
+            moves.AddRange (OneJump (startField, board, (startField.x + 2, startField.y - 2), (startField.x + 1, startField.y - 1), previousPlace));
+
+            return moves;
+        }
+
+        private List<FieldInBoard> OneJump (FieldInBoard startField, Board board, (int X, int Y) newField, (int X, int Y) interField, FieldInBoard previousPlace)
+        {
+            List<FieldInBoard> moves = new List<FieldInBoard> ();
+
+            if (newField.X < Board.side && newField.X >= 0 && newField.Y < Board.side && newField.Y >= 0 &&
+                previousPlace.x != newField.X && previousPlace.y != newField.Y &&
+                board.fields[interField.X, interField.Y].playerOnField != PlayerColor.None &&
+                board.fields[newField.X, newField.Y].playerOnField == PlayerColor.None)
+            {
+                moves.Add (board.fields[newField.X, newField.Y]);
+                moves.AddRange (GetPossibleFieldsThroughJumps (board.fields[newField.X, newField.Y], board, startField));
+            }
+
+            return moves;
+        }
+
+        private List<FieldInBoard> GetPossibleFieldsOneApart (int i, PlayerColor color, Board board)
+        {
+            FieldInBoard checkerField = board.FindCheckersPosition (i, color);
+
+            List<FieldInBoard> possibleFields = new List<FieldInBoard> ();
+            if (checkerField.x + 1 < Board.side && board.fields[checkerField.x + 1, checkerField.y].playerOnField == PlayerColor.None)
+            {
+                possibleFields.Add (board.fields[checkerField.x + 1, checkerField.y]);
+            }
+            if (checkerField.x - 1 >= 0 && board.fields[checkerField.x - 1, checkerField.y].playerOnField == PlayerColor.None)
+            {
+                possibleFields.Add (board.fields[checkerField.x - 1, checkerField.y]);
+            }
+            if (checkerField.y + 1 < Board.side && board.fields[checkerField.x, checkerField.y + 1].playerOnField == PlayerColor.None)
+            {
+                possibleFields.Add (board.fields[checkerField.x, checkerField.y + 1]);
+            }
+            if (checkerField.y - 1 >= 0 && board.fields[checkerField.x, checkerField.y - 1].playerOnField == PlayerColor.None)
+            {
+                possibleFields.Add (board.fields[checkerField.x, checkerField.y - 1]);
+            }
+            if (checkerField.x + 1 < Board.side && checkerField.y - 1 >= 0 && board.fields[checkerField.x + 1, checkerField.y - 1].playerOnField == PlayerColor.None)
+            {
+                possibleFields.Add (board.fields[checkerField.x + 1, checkerField.y - 1]);
+            }
+            if (checkerField.x - 1 >= 0 && checkerField.y + 1 < Board.side && board.fields[checkerField.x - 1, checkerField.y + 1].playerOnField == PlayerColor.None)
+            {
+                possibleFields.Add (board.fields[checkerField.x - 1, checkerField.y + 1]);
+            }
+
+            return possibleFields;
+        }
+
+        private double CalculateHeuristic (FieldInBoard newField, int checkerId, Board board, bool jump, PlayerColor color, FieldInBoard oldField)
+        {
+            double heuristic = 0;
+
+            if (jump)
+            {
+                heuristic += jumpH;
+            }
+
+            if (oldField.fieldType == color && newField.fieldType != color)
+            {
+                heuristic += triangleH;
+            }
+
+            if (newField.fieldType != color && newField.fieldType != PlayerColor.None && (oldField.fieldType == PlayerColor.None || oldField.fieldType == color))
+            {
+                heuristic += opponentTriangleH;
+            }
+
+            // patrzymy na pola o 1 i 2 odsunięte od newField w liniach prostych
+            for (int i = 1; i < distance; i++)
+            {
+                if (newField.x + i < Board.side && board.fields[newField.x + i, newField.y].playerOnField == color)
+                {
+                    if (i <= strongDist)
+                        heuristic += strongDistH;
+                    else
+                        heuristic += distH;
+                }
+                if (newField.x - i >= 0 && board.fields[newField.x - i, newField.y].playerOnField == color)
+                {
+                    if (i <= strongDist)
+                        heuristic += strongDistH;
+                    else
+                        heuristic += distH;
+                }
+                if (newField.y + i < Board.side && board.fields[newField.x, newField.y + i].playerOnField == color)
+                {
+                    if (i <= strongDist)
+                        heuristic += strongDistH;
+                    else
+                        heuristic += distH;
+                }
+                if (newField.y - i >= 0 && board.fields[newField.x, newField.y - i].playerOnField == color)
+                {
+                    if (i <= strongDist)
+                        heuristic += strongDistH;
+                    else
+                        heuristic += distH;
+                }
+                if (newField.x + i < Board.side && newField.y - i >= 0 && board.fields[newField.x + i, newField.y - i].playerOnField == color)
+                {
+                    if (i <= strongDist)
+                        heuristic += strongDistH;
+                    else
+                        heuristic += distH;
+                }
+                if (newField.x - i >= 0 && newField.y + i < Board.side && board.fields[newField.x - i, newField.y + i].playerOnField == color)
+                {
+                    if (i <= strongDist)
+                        heuristic += strongDistH;
+                    else
+                        heuristic += distH;
+                }
+            }
+
+
+            return heuristic;
+        }
     }
 }
